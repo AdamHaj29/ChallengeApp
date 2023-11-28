8878338547d9a3c34bb0889acc7be68dd2ad3065
AdamHaj29 <adam.haajduk@gmail.com>
2023-11-28 22:23:10 +0100

    Zadanie domowe dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,4 +1,23 @@
-﻿
+﻿var name = "Ew";
+var age = 50;
+var gender = "Kobieta";
 
 
-Console.WriteLine("Hello, World!");
+
+
+if(gender == "Kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (gender == "Mężczyzna" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+else
+{
+    Console.WriteLine("Żaden warunek nie został spełniony");
+}
